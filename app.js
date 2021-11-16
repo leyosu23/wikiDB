4a8e8865abb95618bc74ad124ec1dddbf5ea3281
leyosu23 <leyosu23@gmail.com>
2021-11-15 21:11:15 -0500

    Update post article request

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb:localhost:27017/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
 
 const articleSchema = {
   title: String,
@@ -23,7 +23,33 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-//TODO
+app.get("/articles", function(req, res){
+  Article.find(function(err, foundArticle){
+    if(!err){
+      res.send(foundArticle);
+    }
+    else{
+      res.send(err);
+    }
+  })
+})
+
+app.post("articles", function(req, res){
+  console.log(req.body.title);
+  console.log(req.body.content);
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+  newArticle.save(function(err){
+    if (!err){
+      res.send("Successfully added a new article");
+    } else {
+      req.send(err);
+    }
+  });
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
